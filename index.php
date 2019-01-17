6bc9bda38ba5e8f8011faa17f49ae265847ed87d
nguyenminh <minhcshap6288@gmail.com>
2019-01-18 01:21:07 +0700

    manh

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+		echo "Nguyễn Hoàng Anh ";
+		echo "<br>";
+		echo "T1801A";
+		
+		$a =1;
+		echo $a;
+	?>
+	
+</body>
+</html>
